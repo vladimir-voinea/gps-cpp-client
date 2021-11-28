6ad6029eb09d9e6224209944c16f3b1f75ba45ee
Vladimir Voinea <voineavladimir@gmail.com>
2021-11-28 13:42:07 +0200

    works on rpi

--- build.sh
+++ build.sh
@@ -1,3 +1,10 @@
+arch=$(uname -i)
+if [[ $arch == x86_64* ]]; then
+    BASE_IMAGE_VERSION="1.3.0-ubuntu-dynamic"
+elif  [[ $arch == arm* ]]; then
+    BASE_IMAGE_VERSION="1.3.0-ubuntu-dynamic-armhf"
+fi
+
 IMAGE_NAME="gps_service"
 
 APP1_NAME="gps_service_app"
@@ -8,7 +15,7 @@ APP2_PATH_IN_BUILD_IMAGE="/source/build/src/gps_service_client_app/gps_service_c
 
 #################
 echo Building $IMAGE_NAME:build
-docker build -t $IMAGE_NAME:build . -f Dockerfile.build --progress plain
+docker build --build-arg BASE_IMAGE_VERSION=$BASE_IMAGE_VERSION -t $IMAGE_NAME:build . -f Dockerfile.build --progress plain
 
 #################
 docker container create --name extract $IMAGE_NAME:build  
@@ -17,14 +24,14 @@ docker container create --name extract $IMAGE_NAME:build
 docker container cp extract:$APP1_PATH_IN_BUILD_IMAGE ./app
 
 echo Building $APP1_NAME:latest
-docker build -t $APP1_NAME:latest . --progress plain
+docker build --build-arg BASE_IMAGE_VERSION=$BASE_IMAGE_VERSION -t $APP1_NAME:latest . --progress plain
 rm ./app
 
 #################
 docker container cp extract:$APP2_PATH_IN_BUILD_IMAGE ./app
 
 echo Building $APP2_NAME:latest
-docker build -t $APP2_NAME:latest . --progress plain
+docker build --build-arg BASE_IMAGE_VERSION=$BASE_IMAGE_VERSION -t $APP2_NAME:latest . --progress plain
 rm ./app
 
 #################
